afda1cf760bc630e320a8a623c1b75f7d3d3c736
enginzeybek <135993110+enginzeybek@users.noreply.github.com>
2024-10-11 19:11:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int sayi1,sayi2;
+
+sayi1 = 4;
+
+sayi1 += 5;
+
+sayi2 = sayi1;
+
+int sonuc = sayi1 * sayi2;
+
+//Console.WriteLine("sonuç: " + sonuc);
+
+string ad,soyad;
+
+ad = "İREM";
+
+soyad = "AYTEPE";
+
+Console.WriteLine(ad + soyad);
+
+Console.Write("Doctor Who");
+
+int yas = 32;
+
+Console.WriteLine("YAŞ: " + yas);
+
+bool IsSuccess = true;
+bool IsFail = false;
+
+Console.WriteLine(IsSuccess);
+Console.WriteLine(IsFail);
+
+
+
+
+
+
